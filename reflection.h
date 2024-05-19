f1f491ac754fe0502d8c7578e223379468874be3
sofa <babalark@proton.me>
2024-05-20 04:18:37 +1200

    License under LGPL v2.1

--- reflection.h
+++ reflection.h
@@ -1,3 +1,21 @@
+/* libcry - A C testing framework that doesn' make you want to cry
+ * Copyright (C) 2024  babalark <babalark@proton.me>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
 #pragma once
 #ifndef __REFLECTION_H__
 #define __REFLECTION_H__
@@ -5,7 +23,6 @@
 #include <mqueue.h>
 typedef void(impl_func)(mqd_t);
 typedef char *(desc_func)(void);
-
 
 /* linked list node */
 struct test_node {
@@ -16,7 +33,7 @@ struct test_node {
 	struct test_node *next;
 };
 
-/* doubly linked list for storing all the function names */
+/* singly linked list for storing all the function names */
 struct test_list {
 	struct test_node *head;
 };
@@ -26,6 +43,5 @@ void _cry_find_tests(struct test_list *list);
 
 /* Free all the nodes of the list. */
 void _cry_free_list(struct test_list *list);
-
 
 #endif /* __REFLECTION_H__ */
