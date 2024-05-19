f67408ccc47455d149ecf51eeb68ce013a32e2eb
sofa <babalark@proton.me>
2024-05-19 20:59:21 +1200

    mmap /proc/self/exe and get shdrs.
    Getting really close..

--- reflection.c
+++ reflection.c
@@ -1,11 +1,11 @@
 #include "reflection.h"
 #include <elf.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#define __USE_GNU
-#include <dlfcn.h>
-#include <link.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 /**
  * Mallocs and sets up a new node
@@ -23,74 +23,113 @@ static struct test_node *new_node(char *name, impl_func *impl,
 }
 
 /** Uses the base addr to grab the elf header. */
-static Elf64_Ehdr *get_ehdr(struct dl_phdr_info *info) {
-	return (Elf64_Ehdr *)info->dlpi_addr;
+// static Elf64_Ehdr *get_ehdr(struct dl_phdr_info *info) {
+// 	return (Elf64_Ehdr *)info->dlpi_addr;
+// }
+//
+/** Uses the ehdr data to grab the section header.
+ * ehdr must point to the start of the elf.
+ */
+static Elf64_Shdr *get_shdr(Elf64_Ehdr *ehdr) {
+	return (Elf64_Shdr *)((void *)ehdr + ehdr->e_shoff);
 }
-
-/** Uses the base info and ehdr to grab the section header. */
-static Elf64_Shdr *get_shdr(struct dl_phdr_info *info, Elf64_Ehdr *ehdr) {
-	return (Elf64_Shdr *)(info->dlpi_addr + ehdr->e_shoff);
-}
-
-
-static int find_symbols_64(struct dl_phdr_info *info, size_t size,
-                           struct test_list *list) {
-	Elf64_Ehdr *ehdr = get_ehdr(info);
-
+//
+static int find_symbols_64(Elf64_Ehdr *ehdr, struct test_list *list) {
 	if (sizeof(Elf64_Shdr) != ehdr->e_shentsize) {
-		fprintf(stderr, "Elf64_Shdr is different than e_shentsize. Unsure how to proceed!\n");
+		fprintf(stderr, "Elf64_Shdr is different than e_shentsize. Unsure how "
+		                "to proceed!\n");
 		exit(6);
 	}
 
-	Elf64_Shdr *shdrs = get_shdr(info, ehdr);
+	Elf64_Shdr *shdrs = get_shdr(ehdr);
 
-	// FIXME: Segfaulting currently because i guess it doesn't map the whole elf
+	// List types for debugging purposes
 	for (int i = 0; i < ehdr->e_shnum; i++) {
-		printf("%d. 0x%X\n",i, shdrs[i].sh_type);
+		printf("%d. 0x%X\n", i, shdrs[i].sh_type);
 	}
 
 	return 0;
 }
 
-static int phdr_callback(struct dl_phdr_info *info, size_t size, void *data_) {
-	// We only want to run this callback once.
-	// The first time, it's our phdr.
-	// We can just set our data to null to signal this
-	struct test_list **data = data_;
-	if (*data == NULL)
-		return 1;
+/* Populates the given list with symbols. Both its pointers should be null.
+ * Will exit on any errors.
+ * XXX: Should this exit() or return an error that can be reported in TAP?
+ */
+static void find_symbols(struct test_list *list) {
+	// This is a little cursed, but we're gonna parse ourselves.
+	// If you can think of a better more libc way of doing this that isn't
+	// just straight up raw reading the ELF, let me know!
+	int fd = open("/proc/self/exe", O_RDONLY);
+	if (fd < 0) {
+		fprintf(stderr, "Unable to inspect self via /proc/self/exe\n");
+		perror("open");
+		exit(1);
+	}
 
-	struct test_list *list = *data;
-	*data = NULL;
+	struct stat buf;
+	fstat(fd, &buf);
 
-	// different architectures have different alignments.....
-	// It's okay to read the 64 bit one for both this bc these
-	// bytes match up.
-	switch (get_ehdr(info)->e_ident[EI_CLASS]) {
+	Elf64_Ehdr *ehdr = mmap(NULL, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	if (ehdr == NULL || ehdr == MAP_FAILED) {
+		fprintf(stderr, "Failed to mmap /proc/self/exe\n");
+		perror("mmap");
+		close(fd);
+		exit(1);
+	}
+
+	// Check magic bytes
+	if (ehdr->e_ident[0] != 0x7F || ehdr->e_ident[1] != 'E' ||
+	    ehdr->e_ident[2] != 'L' || ehdr->e_ident[3] != 'F') {
+		fprintf(stderr,
+		        "You've somehow managed to run this as not an ELF file. "
+		        "Impressive, but no supported\n");
+
+		munmap(ehdr, buf.st_size);
+		close(fd);
+		exit(42);
+	}
+
+	switch (ehdr->e_ident[EI_CLASS]) {
 	case ELFCLASS64:
-		return find_symbols_64(info, size, list);
+		find_symbols_64(ehdr, list);
+		break;
 	case ELFCLASS32:
 		fprintf(stderr, "Sorry!! 32bit isn't supported currently. "
 		                "If you really do need it, please open an issue!\n");
+
+		munmap(ehdr, buf.st_size);
+		close(fd);
 		exit(1);
 		break;
 	default:
 		fprintf(stderr, "What a strange architecture... or I misread a byte. "
 		                "Either way, bye !\n");
-		fprintf(stderr, "If your ELF is not malformed, please open an issue!\n");
+		fprintf(stderr,
+		        "If your ELF is not malformed, please open an issue!\n");
+
+		munmap(ehdr, buf.st_size);
+		close(fd);
 		exit(4);
 	}
 
-	return 0;
+	// Technically we can still keep going, right?
+	if (munmap(ehdr, buf.st_size) < 0) {
+		perror("munmap");
+	}
+
+	if (close(fd) < 0) {
+		perror("close");
+	}
 }
 
 void _cry_find_tests(struct test_list *list) {
+	find_symbols(list);
+
 	// https://stackoverflow.com/questions/2694290/returning-a-shared-library-symbol-table/2694373#2694373
-	void *handle = dlopen(NULL, RTLD_LAZY);
-
-	dl_iterate_phdr(phdr_callback, &list);
-
-	dlclose(handle);
+	// TODO:
+	// void *handle = dlopen(NULL, RTLD_LAZY);
+	//
+	// dlclose(handle);
 }
 
 void _cry_free_list(struct test_list *list) {
