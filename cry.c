79d70b019d9f294c898b3bf9553f9471c46e64de
babalark <babalark@proton.me>
2024-05-25 20:56:26 +1200

    Basic CLI: Use getopt and list tests

--- cry.c
+++ cry.c
@@ -20,6 +20,7 @@
 #include "reflection.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 static void run_tests(struct test_list *list) {
 	printf("TAP version 14\n");
@@ -42,27 +43,54 @@ static void run_tests(struct test_list *list) {
 	}
 }
 
-int main() {
-	struct test_list tests = {NULL, 0};
+static void print_help() {
+	printf("TODO: Help..\n");
+}
 
-	_cry_find_tests(&tests);
+static void list_tests(struct test_list *list) {
+	if (list->count == 0) {
+		printf("No tests found\n");
+		return;
+	}
 
-	// TODO: Actual nice command line interface
+	// This could be a log10 but i'd like to not link math if i can avoid it.
+	int digits = 0;
+	{
+		int count = list->count;
+		do {
+			digits++;
+		} while (count /= 10);
+	}
 
 	int idx = 1;
-	if (tests.head == NULL)
-		printf("No tests found\n");
-	else
-		for (struct test_node *n = tests.head; n != NULL; n = n->next, idx++) {
-			printf("%d. %s\t", idx, n->name);
-			if (n->desc != NULL) {
-				char *desc = (*n->desc)();
-				printf("%s", desc);
-			} else {
-				printf("(no description)");
-			}
-			putchar('\n');
+	for (struct test_node *n = list->head; n != NULL; n = n->next, idx++) {
+		char *desc = "(no description)";
+		if (n->desc != NULL) {
+			desc = (*n->desc)();
 		}
+
+		printf("%*d. %s - %s\n", digits, idx, n->name, desc);
+	}
+}
+
+int main(int argc, char *argv[]) {
+	struct test_list tests = {NULL, 0};
+	_cry_find_tests(&tests);
+
+	int opt;
+	while ((opt = getopt(argc, argv, "hl")) != -1) {
+		switch (opt) {
+		case 'h':
+		case '?':
+			printf("Help");
+			exit(1);
+			break;
+		case 'l':
+			list_tests(&tests);
+			exit(0);
+			break;
+		}
+	}
 
 	run_tests(&tests);
 
